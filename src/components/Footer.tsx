3e3aed95f9a55f432672d49ed1754774500f049c
zweckllc <132263425+zweckllc@users.noreply.github.com>
2024-11-20 09:54:00 -0800

    Add files via upload

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+	return (
+		<footer className='md:px-8 md:py-0 border-t'>
+			<div className='container flex  items-center gap-4 h-24'>
+				<p className='text-balance text-center text-sm leading-loose text-muted-foreground md:text-left'>
+				© Copyright FansSecret
+				
+				
+					
+				</p>
+			</div>
+		</footer>
+	);
+};
+export default Footer;
